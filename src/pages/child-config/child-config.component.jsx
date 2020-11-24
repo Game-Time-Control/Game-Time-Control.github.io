fe039e3bd7fcb424ceb78bc12e785d86f930c2a7
julia <juliagabriela98@gmail.com>
2020-11-24 16:21:49 -0300

    initial folder structure

---
+++ src/pages/child-config/child-config.component.jsx
@@ -0,0 +1,12 @@
+import React, {useContext} from "react";
+
+export default function ChildConfigPage() {
+
+    return (
+        <div style={{display: "flex", justifyContent: "center"}}>
+            <b>
+            Oi prof Andrea
+            </b>
+        </div>
+    );
+}

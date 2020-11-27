578c85c43e5a7c6bbb6bac1ecd9351aa15151de3
julia <juliagabriela98@gmail.com>
2020-11-27 18:39:10 -0300

    added tabs in child configs

--- src/pages/child-config/child-config.component.jsx
+++ src/pages/child-config/child-config.component.jsx
@@ -1,13 +1,70 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 /* Components */
 import Header from "../../components/header/header.component";
 
-export default function ChildConfigPage() {
+/* Material UI */
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
 
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+export default function ChildConfigPage() {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
     return (
         <div>
             <Header/>
+            <AppBar position="static">
+                <Tabs value={value} onChange={handleChange} aria-label="simple tabs example" centered>
+                    <Tab label="Calendário" {...a11yProps(0)} />
+                    <Tab label="Jogos" {...a11yProps(1)} />
+                </Tabs>
+            </AppBar>
+            <TabPanel value={value} index={0}>
+                Controle da criança
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                Crontrole dos jogos
+            </TabPanel>
         </div>
     );
 }
